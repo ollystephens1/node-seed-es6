a9e4878e602f0135b3bce313ea6c45484c842e14
Olly Stephens <ostephens88@hotmail.co.uk>
2018-03-11 00:18:59 +0100

    Cleaned server.js

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 import express from 'express';
 
-const app = express();
 const PORT = 3000;
+const app = express();
 
 app.get('/', (req, res) => res.send('This is a Node.js API seed Webpack & Babel with ES6'));
-app.listen(PORT, () => console.log('Node API running on port ' + PORT));
+app.listen(PORT, () => console.log(`Node API running on port ${PORT}`));
