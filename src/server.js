7d5f256d9f1cfa869f35989764c959f88f7e5823
Olly Stephens <ostephens88@hotmail.co.uk>
2018-03-11 01:45:48 +0100

    Sorted environment variables

--- src/server.js
+++ src/server.js
@@ -3,5 +3,7 @@ import express from 'express';
 const PORT = 3000;
 const app = express();
 
+console.log('YES');
+
 app.get('/', (req, res) => res.send('This is a Node.js API seed Webpack & Babel with ES6'));
 app.listen(PORT, () => console.log(`Node API running on port ${PORT}`));
