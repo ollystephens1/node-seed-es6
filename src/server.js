99ec2587e4403683ec72c5091fca08344a499e82
Olly Stephens <ostephens88@hotmail.co.uk>
2018-03-13 00:35:17 +0100

    Added morgan & cookie parser

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import config from 'config';
 import helmet from 'helmet';
+import logger from 'morgan';
+import cookieParser from 'cookie-parser';
 import bodyParser from 'body-parser';
 import routes from './routes';
 
@@ -8,6 +10,8 @@ const PORT = config.get('server.port');
 const app = express();
 
 app.use(helmet());
+app.use(logger('dev'));
+app.use(cookieParser());
 app.use(bodyParser.json({
   limit: config.get('server.bodyParserLimit')
 }));
