9db1fe91834919f523bd3c3387cd213913e2dc64
Olly Stephens <ostephens88@hotmail.co.uk>
2018-03-18 23:44:22 +0100

    Added compression middleware

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import config from 'config';
 import helmet from 'helmet';
 import logger from 'morgan';
+import compression from 'compression';
 import cookieParser from 'cookie-parser';
 import bodyParser from 'body-parser';
 import routes from './routes';
@@ -11,10 +12,12 @@ const app = express();
 
 app.use(helmet());
 app.use(logger('dev'));
+app.use(compression());
 app.use(cookieParser());
 app.use(bodyParser.json({
   limit: config.get('server.bodyParserLimit')
 }));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/', routes);
 app.listen(PORT, () => console.log(`Node API running on port ${PORT}`));
